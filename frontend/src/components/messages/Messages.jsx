18f0d5868d15f936be52acee9d4a4d1a48f5419e
houssemchatibi <houssem.chatibi@gmail.com>
2024-03-04 21:00:49 +0100

    Get messages

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,20 +1,29 @@
+import useGetMessages from "../../hooks/useGetMessages";
+import { useEffect } from "react";
 import Message from "./Message";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
+
+	const { messages, loading } = useGetMessages();
+	useEffect(() => {
+		console.log(messages)
+	}, [messages]);
 	return (
+
 		<div className='px-4 flex-1 overflow-auto'>
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} >
+						<Message message={message} />
+					</div>
+				))}
+			{!loading && messages.length === 0 && (
+				<p className='text-center text-white'>Send a message to start the conversation</p>
+			)}
+
 		</div>
 	);
 };
