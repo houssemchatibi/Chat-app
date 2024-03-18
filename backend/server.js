1f272e57d83f516a789be286a9738d76390e05a8
houssemchatibi <houssem.chatibi@gmail.com>
2024-03-18 16:16:28 +0100

    Deployement

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -17,6 +18,7 @@ const corsOptions = {
     credentials: true, // Allow cookies
 };
 
+const __dirname= path.resolve();
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +27,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 
 server.listen(PORT, () => {connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
